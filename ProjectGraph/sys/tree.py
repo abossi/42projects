8e2d804b35bc1881b1b8f8d14d3906ecbb7d2c42
MoutonSanglant <defresne.thierry@gmail.com>
2017-09-25 22:45:59 +0200

    Return file list in a JSON instead of a plain string

--- ProjectGraph/sys/tree.py
+++ ProjectGraph/sys/tree.py
@@ -1,15 +1,16 @@
 import os
+import json
 
 from ProjectGraph import filters
 
 def rebuild(filters=filters.default, project_root='.'):
-    paths=""
+    file_list=[]
     for root, dirs, files in os.walk(project_root):
         if '.git' in dirs:
             dirs.remove('.git') # don't visit .git directories
         for file in files:
             filename, ext = os.path.splitext(file)
             if ext in filters:
-                paths += root + '/' + file + ',';
+                file_list.append(root + '/' + file + ',');
 
-    return paths
+    return json.dumps({ 'files': file_list })
